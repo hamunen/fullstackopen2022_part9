63f979a1e38a26cfa6197e55a028c37a13933308
Henri Hamunen <henri.hamunen@sievo.com>
2023-02-25 20:42:10 +0200

    9.2 Exercise calculator

---
+++ bmiCalculator/exerciseCalculator.ts
@@ -0,0 +1,42 @@
+interface ExerciseResult {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+const calculateExercises = (days: number[], target: number): ExerciseResult => {
+  const avgExerciseHours =
+    days.reduce((sum, day) => sum + day, 0) / days.length;
+
+  const successFactor = avgExerciseHours / target;
+  var rating = 1;
+  var ratingDescription = '';
+  if (successFactor < 0.75) {
+    rating = 1;
+    ratingDescription = 'very bad, work harder fatty';
+  } else if (successFactor < 1) {
+    rating = 2;
+    ratingDescription = 'not too bad but could be better';
+  } else {
+    rating = 3;
+    ratingDescription = 'You reached the target! Well done :)';
+  }
+
+  const result = {
+    periodLength: days.length,
+    trainingDays: days.filter((d) => d !== 0).length,
+    success: successFactor >= 1,
+    rating,
+    ratingDescription,
+    target,
+    average: avgExerciseHours,
+  };
+
+  return result;
+};
+
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
